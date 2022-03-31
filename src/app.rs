81b048313b70a0da0547cbd6b3c27fc36ccf63a1
Connor Keane <connor.keane@kxnr.me>
2022-03-31 01:00:01 -0600

    basic UI layout start

--- src/app.rs
+++ src/app.rs
@@ -1,58 +1,49 @@
 use eframe::{egui, epi};
+use native_dialog::{FileDialog, MessageDialog, MessageType};
+use std::path::PathBuf;
+use parquet::file::reader::SerializedFileReader;
 
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[cfg_attr(feature = "persistence", derive(serde::Deserialize, serde::Serialize))]
 #[cfg_attr(feature = "persistence", serde(default))] // if we add new fields, give them default values when deserializing old state
 pub struct TemplateApp {
-    // Example stuff:
-    label: String,
-
-    // this how you opt-out of serialization of a member
-    #[cfg_attr(feature = "persistence", serde(skip))]
-    value: f32,
+    // #[cfg_attr(feature = "persistence", serde(skip))]
+    table: Option<PathBuf>,
 }
 
 impl Default for TemplateApp {
     fn default() -> Self {
         Self {
             // Example stuff:
-            label: "Hello World!".to_owned(),
-            value: 2.7,
+            table: None,
         }
     }
 }
 
 impl epi::App for TemplateApp {
     fn name(&self) -> &str {
-        "eframe template"
+        "parqbench"
     }
 
-    /// Called once before the first frame.
     fn setup(
         &mut self,
         _ctx: &egui::Context,
         _frame: &epi::Frame,
         _storage: Option<&dyn epi::Storage>,
     ) {
-        // Load previous app state (if any).
-        // Note that you must enable the `persistence` feature for this to work.
         #[cfg(feature = "persistence")]
         if let Some(storage) = _storage {
             *self = epi::get_value(storage, epi::APP_KEY).unwrap_or_default()
         }
     }
 
-    /// Called by the frame work to save state before shutdown.
-    /// Note that you must enable the `persistence` feature for this to work.
     #[cfg(feature = "persistence")]
     fn save(&mut self, storage: &mut dyn epi::Storage) {
         epi::set_value(storage, epi::APP_KEY, self);
     }
 
-    /// Called each time the UI needs repainting, which may be many times per second.
-    /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
     fn update(&mut self, ctx: &egui::Context, frame: &epi::Frame) {
-        let Self { label, value } = self;
+        let Self {table} = self;
 
         // Examples of how to create different panels and windows.
         // Pick whichever suits you.
@@ -63,6 +54,17 @@ impl epi::App for TemplateApp {
             // The top panel is often a good place for a menu bar:
             egui::menu::bar(ui, |ui| {
                 ui.menu_button("File", |ui| {
+                    ui.menu_button("About", |ui| {
+                        ui.label("Built with egui");
+                    });
+
+                    if ui.button("Open...").clicked(){
+                        *table = FileDialog::new()
+                            .set_location("~")
+                            .show_open_single_file()
+                            .unwrap();
+                    }
+
                     if ui.button("Quit").clicked() {
                         frame.quit();
                     }
@@ -70,49 +72,37 @@ impl epi::App for TemplateApp {
             });
         });
 
+
+        // show expand panel button
+        // TODO: the collapse doesn't look like it'll work well. Instead, add a vertical set of
+        // TODO: buttons in a narrow panel that will toggle other panels. Images will likely be
+        // TODO: be best for this
         egui::SidePanel::left("side_panel").show(ctx, |ui| {
-            ui.heading("Side Panel");
-
-            ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(label);
+            let response = egui::CollapsingHeader::new("").default_open(true).show(ui, |ui| {});
+            // TODO: filter
+            // TODO: schema
+            ui.vertical_centered(|ui| {
+                ui.label("over");
+                ui.label("under");
             });
-
-            ui.add(egui::Slider::new(value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                *value += 1.0;
-            }
-
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                ui.horizontal(|ui| {
-                    ui.spacing_mut().item_spacing.x = 0.0;
-                    ui.label("powered by ");
-                    ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-                    ui.label(" and ");
-                    ui.hyperlink_to("eframe", "https://github.com/emilk/egui/tree/master/eframe");
-                });
-            });
+            // match response.body_response {
+            //     Some(body_respones) => {ui.label("open");},
+            //     None => {},
+            // }
         });
 
+
+        // TODO: table
         egui::CentralPanel::default().show(ctx, |ui| {
             // The central panel the region left after adding TopPanel's and SidePanel's
-
-            ui.heading("eframe template");
-            ui.hyperlink("https://github.com/emilk/eframe_template");
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/master/",
-                "Source code."
-            ));
-            egui::warn_if_debug_build(ui);
         });
 
-        if false {
-            egui::Window::new("Window").show(ctx, |ui| {
-                ui.label("Windows can be moved by dragging them.");
-                ui.label("They are automatically sized based on contents.");
-                ui.label("You can turn on resizing and scrolling if you like.");
-                ui.label("You would normally chose either panels OR windows.");
-            });
-        }
+        egui::TopBottomPanel::bottom("bottom_panel").show(ctx, |ui| {
+            match table {
+                Some(table) => {ui.label(&format!("{:#?}", table));},
+                None => {ui.label("no file set");},
+            }
+            egui::warn_if_debug_build(ui);
+        });
     }
 }
