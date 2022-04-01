cca034121928e49cda45c39760068a10b95c10d0
Connor Keane <connor.keane@kxnr.me>
2022-03-31 18:36:03 -0600

    make menu panels and toggle

--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,43 @@
 use eframe::{egui, epi};
-use native_dialog::{FileDialog, MessageDialog, MessageType};
+use egui::{Response, WidgetText, Ui};
+use native_dialog::FileDialog;
 use std::path::PathBuf;
-use parquet::file::reader::SerializedFileReader;
+// use parquet::file::reader::SerializedFileReader;
+
+#[derive(PartialEq)]
+#[cfg_attr(feature = "persistence", derive(serde::Deserialize, serde::Serialize))]
+enum MenuPanels { Schema, Info, Filter}
+
+trait ExtraInteractions {
+    fn toggleable_value<Value: PartialEq>(
+        &mut self,
+        current_value: &mut Option<Value>,
+        selected_value: Value,
+        text: impl Into<WidgetText>,
+    ) -> Response;
+}
+
+impl ExtraInteractions for Ui {
+    fn toggleable_value<Value: PartialEq>(
+        &mut self,
+        current_value: &mut Option<Value>,
+        selected_value: Value,
+        text: impl Into<WidgetText>,
+    ) -> Response {
+        let selected = match current_value {
+            Some(value) => *value == selected_value,
+            None => false,
+        };
+        let mut response = self.selectable_label(selected, text);
+        if response.clicked() {
+            *current_value = if selected {None} else {Some(selected_value)};
+            response.mark_changed();
+        }
+        response
+    }
+}
+
+
 
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[cfg_attr(feature = "persistence", derive(serde::Deserialize, serde::Serialize))]
@@ -9,6 +45,7 @@ use parquet::file::reader::SerializedFileReader;
 pub struct TemplateApp {
     // #[cfg_attr(feature = "persistence", serde(skip))]
     table: Option<PathBuf>,
+    menu_panel: Option<MenuPanels>,
 }
 
 impl Default for TemplateApp {
@@ -16,6 +53,7 @@ impl Default for TemplateApp {
         Self {
             // Example stuff:
             table: None,
+            menu_panel: None,
         }
     }
 }
@@ -43,12 +81,7 @@ impl epi::App for TemplateApp {
     }
 
     fn update(&mut self, ctx: &egui::Context, frame: &epi::Frame) {
-        let Self {table} = self;
-
-        // Examples of how to create different panels and windows.
-        // Pick whichever suits you.
-        // Tip: a good default choice is to just keep the `CentralPanel`.
-        // For inspiration and more examples, go to https://emilk.github.io/egui
+        let Self {table, menu_panel} = self;
 
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
             // The top panel is often a good place for a menu bar:
@@ -77,19 +110,38 @@ impl epi::App for TemplateApp {
         // TODO: the collapse doesn't look like it'll work well. Instead, add a vertical set of
         // TODO: buttons in a narrow panel that will toggle other panels. Images will likely be
         // TODO: be best for this
-        egui::SidePanel::left("side_panel").show(ctx, |ui| {
-            let response = egui::CollapsingHeader::new("").default_open(true).show(ui, |ui| {});
-            // TODO: filter
-            // TODO: schema
-            ui.vertical_centered(|ui| {
-                ui.label("over");
-                ui.label("under");
+        egui::SidePanel::left("side_panel").min_width(0f32).resizable(false).show(ctx, |ui| {
+            ui.vertical(|ui| {
+                // TODO: pop out panels with these buttons
+                // tooltips
+                let _ = ui.toggleable_value(menu_panel, MenuPanels::Schema, "\u{FF5B}");
+                let _ = ui.toggleable_value(menu_panel, MenuPanels::Info, "\u{2139}");
+                let _ = ui.toggleable_value(menu_panel, MenuPanels::Filter, "\u{1F50E}");
             });
-            // match response.body_response {
-            //     Some(body_respones) => {ui.label("open");},
-            //     None => {},
-            // }
         });
+
+        match menu_panel {
+            Some(panel) => {
+                match panel {
+                    MenuPanels::Schema => {
+                        egui::SidePanel::left("side_panel").show(ctx, |ui| {
+                            ui.label("schema menu");
+                        });
+                    },
+                    MenuPanels::Info => {
+                        egui::SidePanel::left("side_panel").show(ctx, |ui| {
+                            ui.label("info menu");
+                        });
+                    },
+                    MenuPanels::Filter => {
+                        egui::SidePanel::left("side_panel").show(ctx, |ui| {
+                            ui.label("filter menu");
+                        });
+                    }
+                }
+            },
+            _ => {},
+        }
 
 
         // TODO: table
