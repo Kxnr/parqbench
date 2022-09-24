622cce8988f0f97e58d8958ea5b5e3e7bd0e05f2
Connor Keane <connor.keane@kxnr.me>
2022-09-23 23:50:52 -0600

    remove additional check

--- check.sh
+++ check.sh
@@ -6,4 +6,3 @@ cargo check --workspace --all-targets
 cargo fmt --all -- --check
 cargo clippy --workspace --all-targets --all-features --  -D warnings -W clippy::all
 cargo test --workspace --all-targets --all-features
-cargo test --workspace --doc
